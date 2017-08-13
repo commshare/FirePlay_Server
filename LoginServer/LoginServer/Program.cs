df264c009687c2417250525f590801107f8b51eb
Lee Kwenwon <highfence@naver.com>
2017-08-13 12:42:48 +0900

    [ADD] Program start code

--- LoginServer/LoginServer/Program.cs
+++ LoginServer/LoginServer/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
 
 namespace LoginServer
 {
@@ -10,6 +12,19 @@ namespace LoginServer
 	{
 		static void Main(string[] args)
 		{
+			string baseAddress = "http://*.19000/";
+
+			using (WebApp.Start<Startup>(url: baseAddress))
+			{
+				var client = new HttpClient();
+
+				var response = client.GetAsync(baseAddress + "api/values").Result;
+
+				Console.WriteLine(response);
+				Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+				Console.ReadLine();
+			}
 		}
 	}
 }
