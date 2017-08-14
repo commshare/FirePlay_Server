56c47248b8e740e1e606377b7bfb0bbd5d6fef73
Lee Kwenwon <highfence@naver.com>
2017-08-14 15:33:35 +0900

    [ADD] 패스워드를 이제 암호화해서 저장, 조회

--- LoginServer/LoginServer/Request/LoginController.cs
+++ LoginServer/LoginServer/Request/LoginController.cs
@@ -12,8 +12,11 @@ namespace LoginServer.Request
 		{
 			var resPacket = new LoginRes();
 
+			// 유저 패스워드를 암호화 한다.
+			var encryptedPassword = Util.Encrypter.EncryptString(reqPacket.UserPw);
+
 			// MongoDB에서 로그인을 요청한 유저의 정보를 찾아본다.
-			var userValidation = await DB.MongoDbManager.GetUserVaildtion(reqPacket.UserId, reqPacket.UserPw);
+			var userValidation = await DB.MongoDbManager.GetUserVaildtion(reqPacket.UserId, encryptedPassword);
 
 			if (userValidation.Result == ErrorCode.None)
 			{
@@ -40,8 +43,13 @@ namespace LoginServer.Request
 
 			var resPacket = new LoginRes();
 
-			var userValidation = await DB.MongoDbManager.JoinUserValidation(signInPacket.UserId, signInPacket.UserPw);
+			// 유저 패스워드를 암호화 한다.
+			var encryptedPassword = Util.Encrypter.EncryptString(signInPacket.UserPw);
 
+			// 회원가입이 가능한 정보인지를 MongoDB에 조회한다.
+			var userValidation = await DB.MongoDbManager.JoinUserValidation(signInPacket.UserId, encryptedPassword);
+
+			// 회원가입이 가능하다면
 			if (userValidation.Result == ErrorCode.None)
 			{
 				resPacket.Result = (short)ErrorCode.None;
@@ -51,6 +59,7 @@ namespace LoginServer.Request
 				// 토큰값을 레디스에 기록한다.
 				await DB.AuthTokenManager.RegistAuthToken(signInPacket.UserId, resPacket.Token);
 			}
+			// 불가능하다면 이유를 적고 반환한다.
 			else
 			{
 				resPacket.Result = (short)userValidation.Result;
