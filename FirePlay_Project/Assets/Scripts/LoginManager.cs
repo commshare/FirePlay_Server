2e6af4fe358b93033dadaaef9787e9d7ff642836
Lee Kwenwon <highfence@naver.com>
2017-08-18 08:25:45 +0900

    [ADD] Buttons

---
+++ FirePlay_Project/Assets/Scripts/LoginManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginManager : MonoBehaviour
+{
+	private void OnGUI()
+	{
+		if (GUI.Button(new Rect(10, 10, 150, 100), "Login"))
+		{
+			
+		}
+
+		if (GUI.Button(new Rect(170, 10, 150, 100), "Exit"))
+		{
+
+		}
+	}
+}
