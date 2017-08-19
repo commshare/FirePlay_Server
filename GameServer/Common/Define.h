c1d39f4711b5c1fc9a07b6a431ed5b8814df58b0
Lee Kwenwon <highfence@naver.com>
2017-08-20 00:56:00 +0900

    [ADD] ServerConfig

--- GameServer/Common/Define.h
+++ GameServer/Common/Define.h
@@ -19,4 +19,21 @@ namespace FirePlayCommon
 		short _bodySize = 0;
 		char * _body = nullptr;
 	};
+
+	class ServerConfig
+	{
+	public :
+
+		ServerConfig() {};
+		~ServerConfig() {};
+
+		unsigned short _port = 0;
+		int _backlog = 0;
+
+		int _maxClientCount = 0;
+		int _extraClientCount = 0;
+
+		short _maxClientRecvSize = 0;
+		short _maxClientSendSize = 0;
+	};
 }
