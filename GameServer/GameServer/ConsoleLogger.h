80d3fcbe716613d6a04c03049dfc72f41a808fa7
Lee Kwenwon <highfence@naver.com>
2017-08-20 00:22:28 +0900

    [ADD] Make Console Logger

--- GameServer/GameServer/ConsoleLogger.h
+++ GameServer/GameServer/ConsoleLogger.h
@@ -1,1 +1,42 @@
 #pragma once
+
+#include "LogBase.h"
+
+namespace FirePlayCommon
+{
+	class ConsoleLogger : public LogBase
+	{
+	public:
+
+		ConsoleLogger() {};
+		virtual ~ConsoleLogger() {};
+
+	protected:
+
+		void error(const char * pText) override
+		{
+			consoleOutWithColor(ConsoleColor::RED, "[ERROR] ", pText);
+		}
+
+		void warn(const char * pText) override
+		{
+			consoleOutWithColor(ConsoleColor::YELLOW, "[WARN] ", pText);
+		}
+
+		void debug(const char * pText) override
+		{
+			consoleOutWithColor(ConsoleColor::WHITE, "[DEBUG] ", pText);
+		}
+
+		void trace(const char * pText) override
+		{
+			consoleOutWithColor(ConsoleColor::BLUE, "[TRACE] ", pText);
+		}
+
+		void info(const char * pText) override
+		{
+			consoleOutWithColor(ConsoleColor::GREEN, "[INFO] ", pText);
+		}
+
+	};
+}
