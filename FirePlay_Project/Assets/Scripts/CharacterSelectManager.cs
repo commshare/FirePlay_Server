f00f9fd180777d5598fe9b02f2c7646ea6e50f7f
Lee Kwenwon <highfence@naver.com>
2017-08-21 21:35:06 +0900

    [ADD] 선택 마네킹 위치 조절

--- FirePlay_Project/Assets/Scripts/CharacterSelectManager.cs
+++ FirePlay_Project/Assets/Scripts/CharacterSelectManager.cs
@@ -13,20 +13,26 @@ public enum PlayerType
 public class CharacterSelectManager : MonoBehaviour
 {
 	public PlayerType _selectedCharacter = PlayerType.Archer1;
-	public Sprite _pointer = null;
+
+	private Sprite _pointer = null;
 	private GameObject _archer1 = null;
 	private GameObject _archer2 = null;
 	private GameObject _archer3 = null;
 
 	private void Start()
 	{
-		LoadArchers();
+		SetArchers();
 	}
 
-	private void LoadArchers()
+	private void SetArchers()
 	{
 		_archer1 = Instantiate(Resources.Load("PrivateData/SpritesArchers/FantasyArcher_01") as GameObject);
+		_archer1.GetComponent<Transform>().position = new Vector3(-4f, 0f, 0f);
+
 		_archer2 = Instantiate(Resources.Load("PrivateData/SpritesArchers/FantasyArcher_02") as GameObject);
-		_archer3 = Instantiate(Resources.Load("PrivateData/SpritesArchers/FantasyArcher_01") as GameObject);
+		_archer2.GetComponent<Transform>().position = new Vector3(0f, 0f, 0f);
+
+		_archer3 = Instantiate(Resources.Load("PrivateData/SpritesArchers/FantasyArcher_03") as GameObject);
+		_archer3.GetComponent<Transform>().position = new Vector3(4f, 0f, 0f);
 	}
 }
