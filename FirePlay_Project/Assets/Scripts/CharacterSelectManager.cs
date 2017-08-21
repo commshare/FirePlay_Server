a4c38a45fb06ae69d120c785b5031fec06b9fdb6
Lee Kwenwon <highfence@naver.com>
2017-08-21 21:38:09 +0900

    [ADD] 선택 모델 멈춰있도록

--- FirePlay_Project/Assets/Scripts/CharacterSelectManager.cs
+++ FirePlay_Project/Assets/Scripts/CharacterSelectManager.cs
@@ -28,11 +28,14 @@ public class CharacterSelectManager : MonoBehaviour
 	{
 		_archer1 = Instantiate(Resources.Load("PrivateData/SpritesArchers/FantasyArcher_01") as GameObject);
 		_archer1.GetComponent<Transform>().position = new Vector3(-4f, 0f, 0f);
+		_archer1.GetComponent<Animator>().enabled = false;
 
 		_archer2 = Instantiate(Resources.Load("PrivateData/SpritesArchers/FantasyArcher_02") as GameObject);
 		_archer2.GetComponent<Transform>().position = new Vector3(0f, 0f, 0f);
+		_archer2.GetComponent<Animator>().enabled = false;
 
 		_archer3 = Instantiate(Resources.Load("PrivateData/SpritesArchers/FantasyArcher_03") as GameObject);
 		_archer3.GetComponent<Transform>().position = new Vector3(4f, 0f, 0f);
+		_archer3.GetComponent<Animator>().enabled = false;
 	}
 }
