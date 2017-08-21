52ef634df291486ce1541a66cc57a511923dd59f
Lee Kwenwon <highfence@naver.com>
2017-08-21 21:12:42 +0900

    [ADD] add datas

---
+++ FirePlay_Project/Assets/Scripts/CharacterSelectManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerType
+{
+	None = 0,
+	Archer1 = 1,
+	Archer2 = 2,
+	Archer3 = 3
+}
+
+public class CharacterSelectManager : MonoBehaviour
+{
+	public PlayerType _selectedCharacter = PlayerType.Archer1;
+	public Sprite _pointer = null;
+	public Animator _archer1 = null; 
+
+	private void Start()
+	{
+		LoadArchers();
+	}
+
+	private void LoadArchers()
+	{
+	}
+}
