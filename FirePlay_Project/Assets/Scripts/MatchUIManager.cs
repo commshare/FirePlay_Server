6296df4f5445542ef2ed4a31516552e48de8401b
Lee Kwenwon <highfence@naver.com>
2017-08-24 09:56:39 +0900

    [UnityClient/ADD] Socket Network

--- FirePlay_Project/Assets/Scripts/MatchUIManager.cs
+++ FirePlay_Project/Assets/Scripts/MatchUIManager.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MatchUIManager : MonoBehaviour {
+public class MatchUIManager : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        MakeButton();
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void MakeButton()
+    {
+        if (GUI.Button(new Rect((Screen.width / 2) - 155, (Screen.height * 2 / 3), 150, 150), "Fast Match"))
+        {
+            TryFastMatch();
+        }
+
+        if (GUI.Button(new Rect((Screen.width / 2) + 5, (Screen.height * 2 / 3), 150, 150), "Number Match"))
+        {
+            // TODO :: Number Match는 추후에 구현.
+        }
+    }
+
+    private void TryFastMatch()
+    {
+
+    }
 }
