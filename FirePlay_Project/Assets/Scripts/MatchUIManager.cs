20c4aa082a01f3c3c7c56fb02c9bca1f6b997496
Lee Kwenwon <highfence@naver.com>
2017-08-25 15:47:24 +0900

    [UnityClient/ADD] Making Game Scene

--- FirePlay_Project/Assets/Scripts/MatchUIManager.cs
+++ FirePlay_Project/Assets/Scripts/MatchUIManager.cs
@@ -11,12 +11,13 @@ public class MatchUIManager : MonoBehaviour
     private GUIStyle _textStyle = null;
     private bool _isTryingNumberMatch = false;
     private bool _isTryingFastMatch = false;
+    private float _matchingCountTime = 0.0f;
+    private string[] _matchingString;
+    private int _matchingCountIdx = 0;
 
     private void Awake()
     {
-        _textStyle = new GUIStyle();
-        _textStyle.fontSize = 40;
-        _textStyle.normal.textColor = Color.white;
+        SetTextOpt();
         _curtain = Instantiate(Resources.Load("Prefabs/BlackCurtain") as GameObject);
         MakeAnim();
     }
@@ -45,29 +46,36 @@ public class MatchUIManager : MonoBehaviour
         {
             if (GUI.Button(new Rect((Screen.width / 2) - 155, (Screen.height * 2 / 3), 150, 150), "Fast Match"))
             {
-                TryFastMatch();
+                _isTryingFastMatch = true;
             }
-
             if (GUI.Button(new Rect((Screen.width / 2) + 5, (Screen.height * 2 / 3), 150, 150), "Number Match"))
             {
-                TryNumberMatch();
+                _curtain.GetComponent<BlackCurtain>().StartFadeIn();
+
+                _isTryingNumberMatch = true;
                 // TODO :: Number Match는 추후에 구현.
             }
         }
         else if (_isTryingNumberMatch == true)
         {
-            GUI.Label(new Rect((Screen.width / 2) - 100, Screen.height / 3, 200, 100), "Matching...", _textStyle);
-
-            if (GUI.Button(new Rect((Screen.width / 2) - 100, Screen.height * 2 / 3, 200, 100), "Cancel"))
-            {
-                _curtain.GetComponent<BlackCurtain>().StartFadeOut();
-                _isTryingNumberMatch = false;
-            }
+            TryNumberMatch();
         }
         else
         {
-
+            TryFastMatch();
         }
+    }
+
+    private void SetTextOpt()
+    {
+        _textStyle = new GUIStyle();
+        _textStyle.fontSize = 40;
+        _textStyle.normal.textColor = Color.white;
+
+        _matchingString = new String[3];
+        _matchingString[0] = "Matching.";
+        _matchingString[1] = "Matching..";
+        _matchingString[2] = "Matching...";
     }
 
     private void TryFastMatch()
@@ -77,9 +85,22 @@ public class MatchUIManager : MonoBehaviour
 
     private void TryNumberMatch()
     {
-        _curtain.GetComponent<BlackCurtain>().StartFadeIn();
+        _matchingCountTime += Time.deltaTime;
 
-        _isTryingNumberMatch = true;
+        if (_matchingCountTime >= 1.0f)
+        {
+            _matchingCountTime = 0f;
+            ++_matchingCountIdx;
+            if (_matchingCountIdx == 3) _matchingCountIdx = 0;
+        }
+
+        GUI.Label(new Rect((Screen.width / 2) - 100, Screen.height / 3, 200, 100), _matchingString[_matchingCountIdx], _textStyle);
+
+        if (GUI.Button(new Rect((Screen.width / 2) - 100, Screen.height * 2 / 3, 200, 100), "Cancel"))
+        {
+            _curtain.GetComponent<BlackCurtain>().StartFadeOut();
+            _isTryingNumberMatch = false;
+        }
     }
 
 }
