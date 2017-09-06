8b62f83854601a83ac59a482d2864c6fade8213c
Lee Kwenwon <highfence@naver.com>
2017-09-06 16:06:24 +0900

    [ADD] HTTP Network Config

--- GameServer/Network/Network/HttpNetwork.cpp
+++ GameServer/Network/Network/HttpNetwork.cpp
@@ -1,14 +1,56 @@
 #include "HttpNetwork.h"
+#include <fstream>
+#include <memory>
+#include <string>
 
 #include "../../Common/ConsoleLogger.h"
 #include "../../Common/Define.h"
+#include "../../Common/json.hpp"
+#include "../../Common/ErrorCode.h"
+
+#include "PacketInfo.h"
 
 namespace FPNetwork
 {
-	void HttpNetwork::Init(ConsoleLogger * logger, ServerConfig * config)
+	using json = nlohmann::json;
+
+	ErrorCode HttpNetwork::Init(ConsoleLogger * logger, PacketQueue * recvQueue)
 	{
 		_logger = logger;
+		_recvQueue = recvQueue;
 
 		// TODO :: DB 서버 주소를 config에서 가져오기.
+		LoadHttpConfig();
 	}
+
+	ErrorCode HttpNetwork::LoadHttpConfig()
+	{
+		std::ifstream configFile("HttpConfig.json");
+		if (configFile.fail())
+		{
+			_logger->Write(LogType::LOG_ERROR, "%s | Config File Load Failed", __FUNCTION__);
+			return ErrorCode::FailConfigLoad;
+		}
+
+		json configJson;
+		configFile >> configJson;
+
+	}
+
+	void HttpNetwork::OnBegin(const happyhttp::Response * r, void * userdata)
+	{
+		_logger->Write(LogType::LOG_DEBUG, "%s | Begin HTTP POST Status(%d : %s)", __FUNCTION__, r->getstatus(), r->getreason());
+	}
+
+	void HttpNetwork::OnData(const happyhttp::Response * r, void * userdata, const unsigned char * data, int n)
+	{
+		auto recvPacket = std::make_shared<PacketInfo>();
+	}
+
+	void HttpNetwork::OnComplete(const happyhttp::Response * r, void * userdata)
+	{
+		_logger->Write(LogType::LOG_DEBUG, "%s | Complete HTTP POST Status(%d : %s)", __FUNCTION__, r->getstatus(), r->getreason());
+	}
+
+
 }
--- GameServer/Network/Network/HttpNetwork.h
+++ GameServer/Network/Network/HttpNetwork.h
@@ -1,18 +1,44 @@
 #pragma once
 #include <string>
+#include <memory>
 
 #include "happyhttp.h"
 
+#include "../../Common/ConsoleLogger.h"
+#include "../../Common/PacketQueue.h"
+#include "../../Common/ErrorCode.h"
+
 namespace FPCommon
 {
-	class ConsoleLogger;
 	class ServerConfig;
+	enum class LogType;
 }
 
 namespace FPNetwork
 {
 	using ConsoleLogger = FPCommon::ConsoleLogger;
+	using PacketQueue = FPCommon::PacketQueue;
 	using ServerConfig = FPCommon::ServerConfig;
+	using LogType = FPCommon::LogType;
+	using ErrorCode = FPCommon::ErrorCode;
+
+	class HttpConfig
+	{
+	public :
+		enum class ApiEnum : int
+		{
+			None = 0,
+			TokenValidation = 1,
+			ApiMaxNum = 2
+		};
+
+		HttpConfig() = default;
+		~HttpConfig() = default;
+
+		std::string _dbServerUrl;
+		int _dbServerPort;
+		std::string _dbServerApi[(int)ApiEnum::ApiMaxNum];
+	};
 
 	/*
 	 * DB 서버와의 Http통신을 전담하는 클래스.
@@ -24,16 +50,32 @@ namespace FPNetwork
 		HttpNetwork() {}
 		~HttpNetwork() {}
 
-		void Init(ConsoleLogger * logger, ServerConfig * config);
+		ErrorCode Init(ConsoleLogger * logger, PacketQueue * recvQueue);
+
+		template <class Request_t>
+		void PostRequest(Request_t req, int reqSessionIdx);
 
 	private :
 
+		ErrorCode LoadHttpConfig();
+
+		void OnBegin(const happyhttp::Response* r, void* userdata);
+
+		void OnData(const happyhttp::Response* r, void* userdata, const unsigned char* data, int n);
+
+		void OnComplete(const happyhttp::Response* r, void* userdata);
 
 	private :
 
-		ConsoleLogger * _logger;
-		std::string     _dbServerUrl;
-		int             _dbServerPort;
+		ConsoleLogger *             _logger;
+		PacketQueue *               _recvQueue;
+		std::unique_ptr<HttpConfig> _config;
 
 	};
+
+	template<class Request_t>
+	inline void HttpNetwork::PostRequest(Request_t req, int reqSessionIdx)
+	{
+		
+	}
 }
